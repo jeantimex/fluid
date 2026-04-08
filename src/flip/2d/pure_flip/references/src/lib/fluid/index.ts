9a9e5211b380cd6ed8084e4b2f792085c64ee31b
Yong Su <jean.timex@gmail.com>
2026-04-08 14:58:39 -0500

    removed refereces

--- src/flip/2d/pure_flip/references/src/lib/fluid/index.ts
+++
@@ -1,3 +0,0 @@
-export { FlipFluid, FLUID_CELL, AIR_CELL, SOLID_CELL } from './FlipFluid';
-export { FluidRenderer, type RenderConfig } from './FluidRenderer';
-export { setupFluidScene, DEFAULT_SCENE_CONFIG, type SceneConfig } from './FluidScene';
